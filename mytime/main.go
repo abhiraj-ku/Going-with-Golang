981f0a0aded551eac3370e741d632b8956c9afca
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-03-03 22:44:00 +0530

    learned about different build for diff os

--- mytime/main.go
+++ mytime/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Welcome to time study")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	// formatting the time
+
+	fmt.Println(presentTime.Format("01-02-2006"))
 }
