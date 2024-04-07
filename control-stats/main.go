e51895e2c7b5fbe7c0a0547ee61c5931180bb65a
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-04-07 22:59:08 +0530

    done with arrays

---
+++ control-stats/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// if-else
+	// in go it is neccessary to have {brakcets even for single line too}
+
+	// in go we can also do declarations before actual if condition
+
+}
