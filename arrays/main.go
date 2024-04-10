22936079da09c9407e9ee38225f573371d4d7063
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-04-10 21:57:57 +0530

    add array final code

--- arrays/main.go
+++ arrays/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Welcome to arrays in Golang!")
@@ -10,7 +8,7 @@ func main() {
 	var b = [3]int{1, 23, 2}
 	fmt.Printf("The memory address of b is:%p\n", &b)
 
-	Assign the array to another variable
+	// Assign the array to another variable
 	var a = b
 	a[0] = 80
 
@@ -18,22 +16,40 @@ func main() {
 	a[2] = 4
 	println(a)
 
-	Print the memory address of the copied array
+	// Print the memory address of the copied array
 	fmt.Println("The a  of a is: \n", &b)
 	fmt.Println("The   b is: \n", &a)
 	names := [5]string{"abhi", "raj", "ken", "seb"}
 	fmt.Println(names, len(names))
 
-	Slices in go
+	// Slices in go
 
 	var a []int         //un initialized slices
 	var b = []int{1, 2} //initalized
 	// a = append(a, 1)
 	b = append(b, 3)
 	b = append(b, 4)
-	fmt.Printf("the add of b is %p\n", &b)
-	b = append(b, 5) //from here on it creates a new array with larger cap and copies to the prev array
-	fmt.Printf("the add of b is %p\n", &b)
+	fmt.Printf("the add of b is %p\n",  &b)
+	b = append(b, 5)     
+	fmt.Printf("The memory address of b is:%p\n", &b)
+
+	// Assign the array to another variable
+	var a = b
+	a[0] = 80
+
+	println("The value of a is:", a)
+	a[2] = 4
+	println(a)
+
+	// Print the memory address of the copied array
+	fmt.Println("The a  of a is: \n", &b)
+	fmt.Println("The   b is: \n", &a)
+	names := [5]string{"abhi", "raj", "ken", "seb"}
+	fmt.Println(names, len(names))
+
+	// Slices in go
+
+	var a []int;        
 	fmt.Println(b, cap(b))
 	fmt.Println(a)
 	fmt.Printf("the add of a is %p\n", &a)
@@ -50,3 +66,42 @@ func main() {
 	fmt.Println(b)
 
 }
+
+// Example of go slice pass by ref and value
+
+func doSome(a [3]int, b []int) []int {
+	a[0] = 4
+	b[0] = 3
+
+	c := make([]int, 5)
+	c[4] = 43
+	copy(c, b)
+
+	return c
+}
+
+// func main() {
+// 	var w = [...]int{1, 2, 3} // array of len(3)
+// 	var x = []int{0, 0, 0}    // slice of len(3)
+
+// 	y := doSome(w, x)
+// 	fmt.Println("Array w:", w)
+// 	fmt.Println("Slice x:", x)
+// 	fmt.Println("Returned slice y:", y)
+// }
+
+// MAPS in Go := it is a dictionary with key and value
+
+// func main() {
+// 	var m = map[string]int{
+// 		"and": 1,
+// 		"the": 1,
+// 		"or":  1,
+// 	}
+
+// 	d := len(m)
+// 	e := m["df"]
+// 	fmt.Println(d)
+// 	fmt.Println(e)
+
+// }
