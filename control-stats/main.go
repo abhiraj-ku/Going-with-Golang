a336ff72c9a9a8b3cb1e8905b97b2039733562aa
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-04-12 18:22:44 +0530

    switch -statement in go completed

--- control-stats/main.go
+++ control-stats/main.go
@@ -9,40 +9,63 @@ func main() {
 	// in go we can also do declarations before actual if condition
 
 	//for loop method -> 1
-	for i := 0; i <= 9; i++ {
-		fmt.Printf("(%d,%d)\n", i, i*i)
-	}
+	// for i := 0; i <= 9; i++ {
+	// 	fmt.Printf("(%d,%d)\n", i, i*i)
+	// }
 
 	//for loop method -> 2 for condition{}
-	sum := 0
-	count := 0
-	max := 100
+	// sum := 0
+	// count := 0
+	// max := 100
 
-	for sum <= max {
-		sum += count
-		count++
-	}
-	countplus := count
-	count++
-	count++
-	fmt.Printf("the value of count: %d and after plus %d", countplus, count)
+	// for sum <= max {
+	// 	sum += count
+	// 	count++
+	// }
+	// countplus := count
+	// count++
+	// count++
+	// fmt.Printf("the value of count: %d and after plus %d", countplus, count)
 
 	//for loop method -> 3 index,value {}
-	fruits := [5]string{"hello", "baby", "how", "are", "you"}
-	fmt.Print(fruits)
+	// fruits := [5]string{"hello", "baby", "how", "are", "you"}
+	// fmt.Print(fruits)
 
-	fruitmap := map[int]string{
-		20: "hello",
-		30: "how",
-		40: "are",
-		50: "ya",
-	}
+	// fruitmap := map[int]string{
+	// 	20: "hello",
+	// 	30: "how",
+	// 	40: "are",
+	// 	50: "ya",
+	// }
 
-	for i, v := range fruitmap {
-		// the order of element in map in go is random so everytime it will be new output
-		fmt.Printf("index:%d value:%s\n", i, v)
-	}
+	// for i, v := range fruitmap {
+	// 	// the order of element in map in go is random so everytime it will be new output
+	// 	fmt.Printf("index:%d value:%s\n", i, v)
+	// }
 
 	// switch-case statements in golang
+
+	// (shik shak shock) golang does not have break keyword after a case it's by default built in
+
+	day := 9
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid day")
+	}
 
 }
