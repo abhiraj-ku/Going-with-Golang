89a9f6e8bbddca35a2a5bbbb6382a20e80dcc1b7
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-04-12 18:27:33 +0530

    fixed array lessons bugs and provided useful comments

--- arrays/main.go
+++ arrays/main.go
@@ -12,73 +12,66 @@ func main() {
 	var a = b
 	a[0] = 80
 
-	println("The value of a is:", a)
+	fmt.Println("The value of a is:", a)
 	a[2] = 4
-	println(a)
-
-	// Print the memory address of the copied array
-	fmt.Println("The a  of a is: \n", &b)
-	fmt.Println("The   b is: \n", &a)
-	names := [5]string{"abhi", "raj", "ken", "seb"}
-	fmt.Println(names, len(names))
-
-	// Slices in go
-
-	var a []int         //un initialized slices
-	var b = []int{1, 2} //initalized
-	// a = append(a, 1)
-	b = append(b, 3)
-	b = append(b, 4)
-	fmt.Printf("the add of b is %p\n",  &b)
-	b = append(b, 5)     
-	fmt.Printf("The memory address of b is:%p\n", &b)
-
-	// Assign the array to another variable
-	var a = b
-	a[0] = 80
-
-	println("The value of a is:", a)
-	a[2] = 4
-	println(a)
-
-	// Print the memory address of the copied array
-	fmt.Println("The a  of a is: \n", &b)
-	fmt.Println("The   b is: \n", &a)
-	names := [5]string{"abhi", "raj", "ken", "seb"}
-	fmt.Println(names, len(names))
-
-	// Slices in go
-
-	var a []int;        
-	fmt.Println(b, cap(b))
 	fmt.Println(a)
-	fmt.Printf("the add of a is %p\n", &a)
-	a = b
-	d := make([]int, 5)
-	fmt.Printf("the type of d is %T\n", d)
+
+	// Print the memory address of the copied array
+	fmt.Println("The address of a is: \n", &b)
+	fmt.Println("The address of b is: \n", &a)
+
+	names := [5]string{"abhi", "raj", "ken", "seb"}
+	fmt.Println(names, len(names))
+
+	// Slices in Go
+
+	var c []int         // uninitialized slice
+	var d = []int{1, 2} // initialized slice
+	d = append(d, 3)
+	d = append(d, 4)
+	fmt.Printf("The memory address of d is:%p\n", &d)
+	d = append(d, 5)
+
+	fmt.Println("The value of d is:", d)
+	d[2] = 4
 	fmt.Println(d)
-	e := a
-	fmt.Printf("the add of e is %p\n", &e)
+
+	// Print the memory address of the copied slice
+	fmt.Println("The address of c is: \n", &c)
+	fmt.Println("The address of d is: \n", &d)
+
+	// Slices in Go
+
+	var aSlice []int
+	fmt.Println(d, cap(d))
+	fmt.Println(aSlice)
+	fmt.Printf("The address of aSlice is %p\n", &aSlice)
+	aSlice = d
+	e := make([]int, 5)
+	fmt.Printf("The type of e is %T\n", e)
 	fmt.Println(e)
-	e[0] = b[0]
-	fmt.Println(e)
-	fmt.Println(fmt.Printf("the add of a is: %p", &a)) // this loc is in go's runtime and not actual mem loc
-	fmt.Println(b)
+	f := aSlice
+	fmt.Printf("The address of f is %p\n", &f)
+	fmt.Println(f)
+	f[0] = d[0]
+	fmt.Println(f)
+	fmt.Println(fmt.Printf("The address of aSlice is: %p", &aSlice)) // this location is in Go's runtime and not actual memory location
+	fmt.Println(d)
 
 }
 
-// Example of go slice pass by ref and value
+// Example of Go slice pass by reference and value
 
-func doSome(a [3]int, b []int) []int {
-	a[0] = 4
-	b[0] = 3
+// func doSome(a [3]int, b []int) []int {
+// 	a[0] = 4
+// 	b[0] = 3
 
-	c := make([]int, 5)
-	c[4] = 43
-	copy(c, b)
+// 	c := make([]int, 5)
+// 	c[4] = 43
+// 	copy(c, b)
 
-	return c
-}
+// 	return c
+// }
 
 // func main() {
 // 	var w = [...]int{1, 2, 3} // array of len(3)
