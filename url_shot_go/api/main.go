490b26068972e3f1c743181dc470c1082d44ca0e
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-06-06 15:25:37 +0530

    Basic setup finished with all dirs

---
+++ url_shot_go/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoute(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.shortenURL)
+
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//server instance of the fiber app
+	app := fiber.New()
+
+	// logger of the server
+	app.Use(logger.New())
+
+	// call the setup route
+
+	setupRoute(app)
+
+	// starting the server
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
--- url_shot_go/api/routes/shorten.go
+++ url_shot_go/api/routes/shorten.go
@@ -1,14 +1,16 @@
 package routes
 
+import "time"
+
 type request struct {
 	URL         string `json:"url"`
 	CustomShort string `json:"short"`
 	Expiry      string `json:"expiry"`
 }
 type response struct {
-	URL             string `json:"url"`
-	CustomShort     string `json:"short"`
-	Expiry          string `json:"expiry"`
-	XRateRemaining  string `json:"rate_limit"`
-	XRateLimitReset string `json:"rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  string        `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
