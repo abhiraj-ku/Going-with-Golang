742217467fa18858b9fd8a3480dddfefb4eaff64
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-06-14 17:29:34 +0530

    added server shutdown code for gracefull closing

--- todo_go/main.go
+++ todo_go/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	// "context"
 	// "encoding/json"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	// "os"
 	// "os/signal"
@@ -13,6 +16,8 @@ import (
 
 	// "github.com/go-chi/chi"
 	// "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/gofiber/fiber/middleware"
 	"github.com/thedevsaddam/renderer"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -61,6 +66,65 @@ func init() {
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"}, nil)
 	checkErr(err)
+}
+
+// function to handle createTodo api
+func createTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	// graceFully handle the server close due to any signal(ctrl+c)
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/todo", todoHandlers())
+
+	// server creation
+	srv := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+	// channel to start the server
+
+	go func() {
+		log.Fatal("server listening on port", port)
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Fatalf("listen :%s\n", err)
+		}
+
+	}()
+
+	// waits for the signal to be sent to stopChan channel if interupt is recieved then only this code will run
+
+	<-stopChan
+	log.Println("Shutting down the server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
+	defer cancel()
+
+	log.Println("Server stoppped gracefully")
+
+}
+
+// todo:= func  todoHandler
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+
+	})
+	return rg
 }
 
 func checkErr(err error) {
