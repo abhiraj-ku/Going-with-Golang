4343e078a3b626c9274b922c2fc23740d6e4cf5d
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-06-23 17:39:54 +0530

    scrapper project setup fixes

--- Rss_feed_scrapper/main.go
+++ Rss_feed_scrapper/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -18,7 +24,39 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		fmt.Println("Environment variable PORT is not set")
-	} else {
-		fmt.Println("Port:", port)
 	}
+
+	// router
+	// for closing the server on ctrl+c gracefully
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	// go channel to start the server
+
+	go func() {
+		fmt.Printf("server started on port: %v", port)
+		srvOn := srv.ListenAndServe()
+
+		if srvOn != nil {
+			log.Fatal(srvOn)
+		}
+		fmt.Println("Port:", port)
+
+	}()
+
+	<-stopChan
+	log.Println("Stopping the server gracefully")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
+	defer cancel()
+
+	log.Println("server stopped gracefully")
+
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/text v0.3.6 // indirect
