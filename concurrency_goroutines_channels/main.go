70dc3f0340bf89e94e929a8d7c532624214e8c6f
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-09-12 12:43:05 +0530

    add csp,goroutines and channels

---
+++ concurrency_goroutines_channels/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// this type will hold the url,err,and time it takes to recieve the data (latency)
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+// make the call to get the data
+func get(url string, ch chan<- result) {
+	start := time.Now()
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0}
+
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
+	}
+}
+
+func main() {
+	result := make(chan result)
+	list := []string{
+		"https://amazon.com",
+		"https://google.com",
+		"https://wsj.com",
+		"https://abhikumar.site",
+	}
+
+	for _, url := range list {
+		go get(url, result)
+	}
+	for range list {
+		r := <-result
+
+		if r.err != nil {
+			log.Printf("%-20s %s\n", r.url, r.err)
+		} else {
+			log.Printf("%-20s %s\n", r.url, r.latency)
+
+		}
+
+	}
+
+}
